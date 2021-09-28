cc14f4324182755390dcf39564a661cc307e1eb1
Dmitriy Kravchenko <dmytro.kravchenko@takeoff.com>
2021-09-28 18:44:22 +0300

    typo in tf resource name with underscore

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
-resource "google_project_service" "apigateway.googleapis.com" {
+resource "google_project_service" "enable_apigateway_service" {
   provider = google
   project  = var.gcp_project_id
   service  = "apigateway.googleapis.com"
