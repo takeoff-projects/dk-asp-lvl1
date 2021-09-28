e6874793d7f5a1ddaf9e4ccfddb8b3164e8fc0e6
Dmitriy Kravchenko <dmytro.kravchenko@takeoff.com>
2021-09-28 18:07:14 +0300

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-    project = var.gcp_project_id
-    region = var.region
-    zone = var.zone
+  project = var.gcp_project_id
+  region  = var.region
+  zone    = var.zone
 }
 locals {
   service_name = "go-pets"
@@ -20,16 +20,16 @@ resource "google_cloud_run_service" "default" {
 
   traffic {
     percent         = 100
-    latest_revision = true    
+    latest_revision = true
   }
 }
 
 resource "google_project_service" "apigateaway.googleapis.com" {
-    provider = google
-    project = var.gcp_project_id
-    service = "apigateway.googleapis.com"
+  provider = google
+  project  = var.gcp_project_id
+  service  = "apigateway.googleapis.com"
 
-    disable_on_destroy = false
+  disable_on_destroy = false
 }
 data "google_iam_policy" "noauth" {
   binding {
@@ -41,45 +41,45 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
 resource "google_datastore_index" "default" {
-    kind = "Pet"
-    properties {
-      name = "added"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "capption"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "email"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "image"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "likes"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "owner"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "petname"
-      direction = "ASCENDING"
-    }
-    properties {
-      name = "Name"
-      direction = "ASCENDING"
-    }
+  kind = "Pet"
+  properties {
+    name      = "added"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "capption"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "email"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "image"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "likes"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "owner"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "petname"
+    direction = "ASCENDING"
+  }
+  properties {
+    name      = "Name"
+    direction = "ASCENDING"
+  }
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,8 +1,8 @@
 # GCP Settings
 gcp_project_id = "roi-takeoff-user54"
-region = "us-central1"
-zone = "us-central1-c"
+region         = "us-central1"
+zone           = "us-central1-c"
 
 # Cloud Run application settings
 server_port = "8080"
-db_name = "asp-go-pets"
+db_name     = "asp-go-pets"
